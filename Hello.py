66f9a68b7d111fe1df28d45d6a12bd0410b27812
Žane Suhadolnik <zanesuhi@gmail.com>
2017-05-26 16:48:53 +0200

    Add files via upload

---
+++ Hello.py
@@ -0,0 +1,13 @@
+print("hello, world")
+
+#while True:
+    #print("hello, world")
+    
+#for i in range(50):
+    #print("hello, world")
+---------------------------------------
+#def main()
+    #print("hello")
+
+#if __name__ == "__main__":
+    #main()
