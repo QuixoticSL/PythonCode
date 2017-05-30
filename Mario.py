6c78d0cb657ecbff642ad5f34d9132c909a3fa54
Zane <zanesuhi@gmail.com>
2017-05-30 20:21:59 +0200

    Added.

---
+++ Mario.py
@@ -0,0 +1,11 @@
+def main(n):
+    assert n in range(1,24)
+    for i in range(2, n+2):
+        print(" "*(n+1-i), end="")
+        
+if __name__ == '__main__':
+    while True:
+        no_of_lines = int(input('Height: ').strip())
+        if no_of_lines in range(1,24):
+            break
+    main(no_of_lines)
